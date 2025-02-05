fea5594a345ba3df45d3bca27d0f01205dc7417b
Mayke Levoni <47572708+maykelevoni@users.noreply.github.com>
2025-02-05 23:04:58 +0000

    adiciona  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Aprendendo programacao
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Mae, Pai amo voces</h1>
+function Home() {
+  return <h1>Mae, Pai amo voces</h1>;
 }
 
 export default Home;
